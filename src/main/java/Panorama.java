f073563004612efe14c315a607be51752f253bb7
weiseng18 <tws.hci@gmail.com>
2024-08-22 01:57:36 +0900

    implement echo and exit

--- src/main/java/Panorama.java
+++ src/main/java/Panorama.java
@@ -1,12 +1,34 @@
+import java.util.Scanner;
+
 public class Panorama {
     public static void main(String[] args) {
-        String separator = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        String separator = "    ____________________________________________________________";
+        // 5 spaces
+        String indent = "     ";
 
         System.out.println(separator);
-        System.out.println(" Hello! I'm Panorama");
-        System.out.println(" What can I do for you?");
+        System.out.println(indent + "Hello! I'm Panorama");
+        System.out.println(indent + "What can I do for you?");
         System.out.println(separator);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        String input;
+        String exit_command = "bye";
+        String exit_statement = "Bye. Hope to see you again soon!";
+
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals(exit_command)) {
+                System.out.println(indent + exit_statement);
+                break;
+            } else {
+              System.out.println(separator);
+              System.out.println(indent + input);
+              System.out.println(separator);
+              System.out.println();
+            }
+        }
+
+        scanner.close();
     }
 }
