e8a8113a12c93348031ad8f0574df8297a08bc6a
weiseng18 <tws.hci@gmail.com>
2024-08-22 01:48:12 +0900

    initial skeletal version

--- src/main/java/Panorama.java
+++ src/main/java/Panorama.java
@@ -1,10 +1,12 @@
 public class Panorama {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String separator = "____________________________________________________________";
+
+        System.out.println(separator);
+        System.out.println(" Hello! I'm Panorama");
+        System.out.println(" What can I do for you?");
+        System.out.println(separator);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
