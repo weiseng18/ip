9d8f23fa7a518c04d898b391ad591fae038c99bf
weiseng18 <tws.hci@gmail.com>
2024-08-31 17:05:38 +0800

    Refactor task handling into TaskManager class

--- src/main/java/Panorama.java
+++ src/main/java/Panorama.java
@@ -1,18 +1,7 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.File;
 import java.io.FileNotFoundException;
-
-// Task folder
-import MyTask.Task;
-import MyTask.Todo;
-import MyTask.Deadline;
-import MyTask.Event;
 
 // Exceptions folder
 import MyException.EmptyDescriptionException;
@@ -21,11 +10,10 @@ import MyException.UnknownCommandException;
 public class Panorama {
 
     // Constants
-    static final String SEPARATOR = "    ____________________________________________________________";
-    static final String INDENT = "     "; // 5 spaces
-    static final String SAVE_FILE_NAME = "./data.txt";
-    // Memory
-    static List<Task> memory;
+    public static final String SEPARATOR = "    ____________________________________________________________";
+    public static final String INDENT = "     "; // 5 spaces
+
+    static TaskManager taskManager;
 
     static void welcome_greeting() {
         System.out.println(SEPARATOR);
@@ -38,84 +26,6 @@ public class Panorama {
         String exit_statement = "Bye. Hope to see you again soon!";
         System.out.println(SEPARATOR);
         System.out.println(INDENT + exit_statement);
-        System.out.println(SEPARATOR);
-    }
-
-    static void add_todo(String name) {
-        memory.add(new Todo(name));
-
-        System.out.println(SEPARATOR);
-        System.out.println(INDENT + "Added task:");
-        System.out.println(INDENT + memory.get(memory.size() - 1));
-        System.out.println(SEPARATOR);
-    }
-
-    static void add_deadline(String name, String date) {
-        memory.add(new Deadline(name, date));
-
-        System.out.println(SEPARATOR);
-        System.out.println(INDENT + "Added task:");
-        System.out.println(INDENT + memory.get(memory.size() - 1));
-        System.out.println(SEPARATOR);
-    }
-
-    static void add_event(String name, String from, String to) {
-        memory.add(new Event(name, from, to));
-
-        System.out.println(SEPARATOR);
-        System.out.println(INDENT + "Added task:");
-        System.out.println(INDENT + memory.get(memory.size() - 1));
-        System.out.println(SEPARATOR);
-    }
-
-    static void list_entries() {
-        System.out.println(SEPARATOR);
-        for (int i = 0; i < memory.size(); i++) {
-            int num = i + 1;
-            System.out.print(INDENT + num + ". ");
-            System.out.println(memory.get(i));
-        }
-        System.out.println(SEPARATOR);
-    }
-
-    static void mark_task(String string_id) {
-        String mark_statement = "Nice! I've marked this task as done:";
-
-        int id = Integer.parseInt(string_id);
-        // because input is 1-indexed
-        memory.get(id - 1).setIsDone(true);
-
-        System.out.println(SEPARATOR);
-        System.out.println(INDENT + mark_statement);
-        System.out.println(INDENT + memory.get(id - 1));
-        System.out.println(SEPARATOR);
-    }
-
-    static void unmark_task(String string_id) {
-        String unmark_statement = "OK, I've marked this task as not done yet:";
-
-        int id = Integer.parseInt(string_id);
-        // because input is 1-indexed
-        memory.get(id - 1).setIsDone(false);
-
-        System.out.println(SEPARATOR);
-        System.out.println(INDENT + unmark_statement);
-        System.out.println(INDENT + memory.get(id - 1));
-        System.out.println(SEPARATOR);
-    }
-
-    static void delete_task(String string_id) {
-        String delete_statement = "Noted. I've removed this task:";
-
-        int id = Integer.parseInt(string_id);
-        Task t = memory.get(id - 1);
-
-        // because input is 1-indexed
-        memory.remove(id - 1);
-
-        System.out.println(SEPARATOR);
-        System.out.println(INDENT + delete_statement);
-        System.out.println(INDENT + t);
         System.out.println(SEPARATOR);
     }
 
@@ -136,52 +46,15 @@ public class Panorama {
         System.out.println(SEPARATOR);
     }
 
-    /**
-     * Load task list from ./data.txt.
-     * If non-existent, assume that there is no past data.
-     */
-    static void load_task_list() throws FileNotFoundException {
-        File myFile = new File(SAVE_FILE_NAME);
-        Scanner myReader = new Scanner(myFile);
-        while (myReader.hasNextLine()) {
-            String task = myReader.nextLine();
-            // Note that | is a special char in regex
-            String[] tokens = task.split("\\|");
-
-            if (tokens[0].equals("T")) {
-                memory.add(new Todo(tokens[2]));
-            } else if (tokens[0].equals("D")) {
-                memory.add(new Deadline(tokens[2], tokens[3]));
-            } else {
-                memory.add(new Event(tokens[2], tokens[3], tokens[4]));
-            }
-
-            // Set marked/unmarked correctly
-            memory.get(memory.size() - 1).setIsDone(tokens[1] == "1");
-        }
-    }
-
-    /**
-     * Save current task list to ./data.txt.
-     */
-    static void save_task_list() throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(SAVE_FILE_NAME));
-        for (int i = 0; i < memory.size(); i++) {
-            writer.write(memory.get(i).toFileString() + "\n");
-        }
-        writer.close();
-    }
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        taskManager = new TaskManager();
 
         welcome_greeting();
-        memory = new ArrayList<Task>();
 
         // Check for past data
         try {
-            load_task_list();
+            taskManager.loadTaskList();
             System.out.println("Successfully loaded task list.");
         } catch (FileNotFoundException e) {
             System.out.println("./data.txt does not exist. Starting from an empty task list.");
@@ -195,66 +68,32 @@ public class Panorama {
             try {
                 input = scanner.nextLine();
                 String[] tokens = input.split(" ");
-                String name;
-
-                // for splitting tokens further
-                String contents;
-                String[] content_tokens;
 
                 switch (tokens[0]) {
                     case "mark":
-                        mark_task(tokens[1]);
+                        taskManager.markTask(tokens[1]);
                         break;
                     case "unmark":
-                        unmark_task(tokens[1]);
+                        taskManager.unmarkTask(tokens[1]);
                         break;
                     case "bye":
                         exit_greeting();
                         hasExited = true;
                         break;
                     case "list":
-                        list_entries();
+                        taskManager.listEntries();
                         break;
                     case "todo":
-                        if (input.length() <= 5) {
-                            throw new EmptyDescriptionException();
-                        }
-
-                        name = input.substring(5);
-                        add_todo(name);
+                        taskManager.addTodoTask(input);
                         break;
                     case "deadline":
-                        if (input.length() <= 9) {
-                            throw new EmptyDescriptionException();
-                        }
-
-                        contents = input.substring(9);
-
-                        content_tokens = contents.split(" /by ");
-                        name = content_tokens[0];
-                        String date = content_tokens[1];
-
-                        add_deadline(name, date);
+                        taskManager.addDeadlineTask(input);
                         break;
                     case "event":
-                        if (input.length() <= 6) {
-                            throw new EmptyDescriptionException();
-                        }
-
-                        contents = input.substring(6);
-
-                        content_tokens = contents.split(" /from ");
-                        name = content_tokens[0];
-
-                        String[] dateRange = content_tokens[1].split(" /to ");
-
-                        String from = dateRange[0];
-                        String to = dateRange[1];
-
-                        add_event(name, from, to);
+                        taskManager.addEventTask(input);
                         break;
                     case "delete":
-                        delete_task(tokens[1]);
+                        taskManager.deleteTask(tokens[1]);
                         break;
                     case "help":
                         display_help();
@@ -273,7 +112,7 @@ public class Panorama {
 
         // Save task list to file after exiting
         try {
-            save_task_list();
+            taskManager.saveTaskList();
             System.out.println("Successfully saved to file.");
         } catch (IOException e) {
             System.out.println("Error in saving to file.");
---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,143 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Scanner;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+// Exceptions folder
+import MyException.EmptyDescriptionException;
+
+// Task folder
+import MyTask.Task;
+import MyTask.Todo;
+import MyTask.Deadline;
+import MyTask.Event;
+
+public class TaskManager {
+    // Constants
+    private static final String SAVE_FILE_NAME = "./data.txt";
+
+    private List<Task> memory;
+
+    public TaskManager() {
+        memory = new ArrayList<>();
+    }
+
+    void addTodoTask(String input) throws EmptyDescriptionException {
+        if (input.length() <= 5) {
+            throw new EmptyDescriptionException();
+        }
+        String name = input.substring(5);
+        memory.add(new Todo(name));
+        printTaskAdded(memory.size() - 1);
+    }
+
+    void addDeadlineTask(String input) throws EmptyDescriptionException {
+        if (input.length() <= 9) {
+            throw new EmptyDescriptionException();
+        }
+        String[] contentTokens = input.substring(9).split(" /by ");
+        String name = contentTokens[0];
+        String date = contentTokens[1];
+        memory.add(new Deadline(name, date));
+        printTaskAdded(memory.size() - 1);
+    }
+
+    void addEventTask(String input) throws EmptyDescriptionException {
+        if (input.length() <= 6) {
+            throw new EmptyDescriptionException();
+        }
+        String[] contentTokens = input.substring(6).split(" /from ");
+        String name = contentTokens[0];
+        String[] dateRange = contentTokens[1].split(" /to ");
+        String from = dateRange[0];
+        String to = dateRange[1];
+        memory.add(new Event(name, from, to));
+        printTaskAdded(memory.size() - 1);
+    }
+
+    void listEntries() {
+        System.out.println(Panorama.SEPARATOR);
+        for (int i = 0; i < memory.size(); i++) {
+            int num = i + 1;
+            System.out.print(Panorama.INDENT + num + ". ");
+            System.out.println(memory.get(i));
+        }
+        System.out.println(Panorama.SEPARATOR);
+    }
+
+    void markTask(String stringId) {
+        int id = Integer.parseInt(stringId) - 1;
+        memory.get(id).setIsDone(true);
+        System.out.println(Panorama.SEPARATOR);
+        System.out.println(Panorama.INDENT + "Nice! I've marked this task as done:");
+        System.out.println(Panorama.INDENT + memory.get(id));
+        System.out.println(Panorama.SEPARATOR);
+    }
+
+    void unmarkTask(String stringId) {
+        int id = Integer.parseInt(stringId) - 1;
+        memory.get(id).setIsDone(false);
+        System.out.println(Panorama.SEPARATOR);
+        System.out.println(Panorama.INDENT + "OK, I've marked this task as not done yet:");
+        System.out.println(Panorama.INDENT + memory.get(id));
+        System.out.println(Panorama.SEPARATOR);
+    }
+
+    void deleteTask(String stringId) {
+        int id = Integer.parseInt(stringId) - 1;
+        Task task = memory.remove(id);
+        System.out.println(Panorama.SEPARATOR);
+        System.out.println(Panorama.INDENT + "Noted. I've removed this task:");
+        System.out.println(Panorama.INDENT + task);
+        System.out.println(Panorama.SEPARATOR);
+    }
+    
+    private void printTaskAdded(int index) {
+        System.out.println(Panorama.SEPARATOR);
+        System.out.println(Panorama.INDENT + "Added task:");
+        System.out.println(Panorama.INDENT + memory.get(index));
+        System.out.println(Panorama.SEPARATOR);
+    }
+
+    /**
+     * Load task list from ./data.txt.
+     * If non-existent, assume that there is no past data.
+     */
+    void loadTaskList() throws FileNotFoundException {
+        File myFile = new File(SAVE_FILE_NAME);
+        Scanner myReader = new Scanner(myFile);
+        while (myReader.hasNextLine()) {
+            String task = myReader.nextLine();
+            // Note that | is a special char in regex
+            String[] tokens = task.split("\\|");
+
+            if (tokens[0].equals("T")) {
+                memory.add(new Todo(tokens[2]));
+            } else if (tokens[0].equals("D")) {
+                memory.add(new Deadline(tokens[2], tokens[3]));
+            } else {
+                memory.add(new Event(tokens[2], tokens[3], tokens[4]));
+            }
+
+            // Set marked/unmarked correctly
+            memory.get(memory.size() - 1).setIsDone(tokens[1] == "1");
+        }
+    }
+
+    /**
+     * Save current task list to ./data.txt.
+     */
+    void saveTaskList() throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(SAVE_FILE_NAME));
+        for (int i = 0; i < memory.size(); i++) {
+            writer.write(memory.get(i).toFileString() + "\n");
+        }
+        writer.close();
+    }
+}
