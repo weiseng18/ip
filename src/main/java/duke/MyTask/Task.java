a1d109f029816c9dd82562b68781b56e56d62b16
weiseng18 <tws.hci@gmail.com>
2024-09-06 14:46:57 +0800

    Add Javadoc for abstract class Task and its subclasses

--- src/main/java/duke/MyTask/Task.java
+++ src/main/java/duke/MyTask/Task.java
@@ -1,23 +1,41 @@
 package duke.MyTask;
 
+/**
+ * Represents an abstract task with a name, status, and type.
+ * Subclasses should extend this class to define specific types of tasks.
+ */
 public abstract class Task {
     String name;
     boolean isDone;
     char c;
 
+    /**
+     * Constructs a {@code Task} with the specified name and type.
+     * The task is initialized as not done.
+     *
+     * @param name The name or description of the task.
+     * @param c The type of the task, represented as a character.
+     */
     public Task(String name, char c) {
         this.name = name;
         this.isDone = false;
         this.c = c;
     }
 
+    /**
+     * Sets the completion status of the task.
+     *
+     * @param status {@code true} if the task is completed, {@code false} otherwise.
+     */
     public void setIsDone(boolean status) {
         this.isDone = status;
     }
 
     /**
-     * Represent Task as a string that is easier for read/write
-     * Note the use of | as the separator.
+     * Returns a string representation of the task in a format suitable for file storage.
+     * The format uses '|' as a separator between fields.
+     *
+     * @return A formatted string for writing the task to a file.
      */
     public String toFileString() {
         String output = "";
@@ -29,6 +47,12 @@ public abstract class Task {
         return output;
     }
 
+    /**
+     * Returns a string representation of the task for displaying in the user interface.
+     * The format includes the task type, completion status, and task name.
+     *
+     * @return A formatted string representing the task.
+     */
     public String toString() {
         String output = "";
         output += "[" + c + "]";
