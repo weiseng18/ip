a181cf3f59f188a2e97c8dfd9c1b8801388bca5a
weiseng18 <tws.hci@gmail.com>
2024-09-08 00:22:41 +0800

    Add happy flow unit test for task creation functions

--- src/test/java/duke/CommandHandlerTest.java
+++ src/test/java/duke/CommandHandlerTest.java
@@ -7,6 +7,7 @@ import duke.MyTask.Todo;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 public class CommandHandlerTest {
     private TaskList initTaskList() {
@@ -49,6 +50,75 @@ public class CommandHandlerTest {
         t = taskList.getMemory().get(0);
         assertEquals(false, t.getIsDone());
     }
+
+    @Test
+    public void handleTodoTest() {
+        TaskList taskList = initTaskList();
+        int previousSize = taskList.getMemory().size();
+
+        String name = "todo_task";
+
+        assertDoesNotThrow(() -> {
+            TaskList newList = CommandHandler.handleTodo(taskList, "todo " + name);
+
+            // List size increases by 1
+            assertEquals(previousSize + 1, newList.getMemory().size());
+
+            // Check last element added
+            List<Task> memory = newList.getMemory();
+            Task last = memory.get(memory.size() - 1);
+            assertEquals(name, last.getName());
+        });
+    }
+
+    @Test
+    public void handleDeadlineTest() {
+        TaskList taskList = initTaskList();
+        int previousSize = taskList.getMemory().size();
+
+        String name = "deadline_task";
+        String validDate = "2024-09-01 2354";
+
+        String query = "deadline " + name + " /by " + validDate;
+
+        assertDoesNotThrow(() -> {
+            TaskList newList = CommandHandler.handleDeadline(taskList, query);
+
+            // List size increases by 1
+            assertEquals(previousSize + 1, newList.getMemory().size());
+
+            // Check last element added
+            List<Task> memory = newList.getMemory();
+            Task last = memory.get(memory.size() - 1);
+            assertEquals(name, last.getName());
+        });
+    }
+
+
+    @Test
+    public void handleEventTest() {
+        TaskList taskList = initTaskList();
+        int previousSize = taskList.getMemory().size();
+
+        String name = "todo_task";
+        String validStartDate = "2024-09-01 2354";
+        String validEndDate = "2024-09-02 1247";
+
+        String query = "event " + name + " /from " + validStartDate + " /to " + validEndDate;
+
+        assertDoesNotThrow(() -> {
+            TaskList newList = CommandHandler.handleEvent(taskList, query);
+
+            // List size increases by 1
+            assertEquals(previousSize + 1, newList.getMemory().size());
+
+            // Check last element added
+            List<Task> memory = newList.getMemory();
+            Task last = memory.get(memory.size() - 1);
+            assertEquals(name, last.getName());
+        });
+    }
+
 
     @Test
     public void handleDeleteTest() {
