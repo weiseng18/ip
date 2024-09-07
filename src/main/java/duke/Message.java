a5f66678bf13e03ba086b6c32ac5063f67f34138
weiseng18 <tws.hci@gmail.com>
2024-09-07 16:44:24 +0800

    Create Message class to contain hardcoded strings

---
+++ src/main/java/duke/Message.java
@@ -0,0 +1,9 @@
+package duke;
+
+public class Message {
+    public class MyException {
+        public static String EMPTY_DESCRIPTION = "Description cannot be empty.";
+        public static String INVALID_DATE = "Invalid date specified.";
+        public static String UNKNOWN_COMMAND = "Unknown command.";
+    }
+}
