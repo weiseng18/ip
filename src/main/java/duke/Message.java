def1bde214afa1e46b629e502b3472c6906f6e46
weiseng18 <tws.hci@gmail.com>
2024-09-08 15:03:45 +0800

    Add final keyword to constants in Message.java

--- src/main/java/duke/Message.java
+++ src/main/java/duke/Message.java
@@ -2,26 +2,26 @@ package duke;
 
 public class Message {
     public class MyException {
-        public static String EMPTY_DESCRIPTION = "Description cannot be empty.";
-        public static String INVALID_DATE = "Invalid date specified.";
-        public static String UNKNOWN_COMMAND = "Unknown command.";
+        public static final String EMPTY_DESCRIPTION = "Description cannot be empty.";
+        public static final String INVALID_DATE = "Invalid date specified.";
+        public static final String UNKNOWN_COMMAND = "Unknown command.";
     }
 
     public class CommandFormat {
         // task listing
-        public static String LIST = "list";
-        public static String FIND = "find (description)";
+        public static final String LIST = "list";
+        public static final String FIND = "find (description)";
 
-        public static String BYE = "bye";
+        public static final String BYE = "bye";
 
         // task creation
-        public static String TODO = "todo (description)";
-        public static String DEADLINE = "deadline (description) /by (date_string)";
-        public static String EVENT = "event (description) /from (date_string) /to (date_string)";
+        public static final String TODO = "todo (description)";
+        public static final String DEADLINE = "deadline (description) /by (date_string)";
+        public static final String EVENT = "event (description) /from (date_string) /to (date_string)";
 
         // task amendment
-        public static String MARK = "mark (task_id)";
-        public static String UNMARK = "unmark (task_id)";
-        public static String DELETE = "delete (task_id)";
+        public static final String MARK = "mark (task_id)";
+        public static final String UNMARK = "unmark (task_id)";
+        public static final String DELETE = "delete (task_id)";
     }
 }
