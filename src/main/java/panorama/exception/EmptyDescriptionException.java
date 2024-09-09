0a28f3fbd8c09717a3cb0697d5d276cffe899e5a
weiseng18 <tws.hci@gmail.com>
2024-09-09 20:04:42 +0800

    Duplicate custom exception classes

---
+++ src/main/java/panorama/exception/EmptyDescriptionException.java
@@ -0,0 +1,17 @@
+package panorama.exception;
+
+import panorama.Message;
+
+/**
+ * Thrown to indicate that a task description is empty.
+ * This exception is used to signal that a user-provided input for a task
+ * does not include a required description.
+ */
+public class EmptyDescriptionException extends Exception {
+    /**
+     * Constructs a new {@code EmptyDescriptionException} with a default error message.
+     */
+    public EmptyDescriptionException() {
+        super(Message.MyException.EMPTY_DESCRIPTION);
+    }
+}
---
+++ src/main/java/panorama/exception/UnknownCommandException.java
@@ -0,0 +1,17 @@
+package panorama.exception;
+
+import panorama.Message;
+
+/**
+ * Thrown to indicate that an unknown command has been issued.
+ * This exception is used to signal that the user has entered a command
+ * that is not recognized by the application.
+ */
+public class UnknownCommandException extends Exception {
+    /**
+     * Constructs a new {@code UnknownCommandException} with a default error message.
+     */
+    public UnknownCommandException() {
+        super(Message.MyException.UNKNOWN_COMMAND);
+    }
+}
