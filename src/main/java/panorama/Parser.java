9fedb3fba868b68103d11b872a81e2f3997b8db1
weiseng18 <tws.hci@gmail.com>
2024-09-09 21:17:35 +0800

    Add Delete, Mark, Unmark

--- src/main/java/panorama/Parser.java
+++ src/main/java/panorama/Parser.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 
 import panorama.command.Command;
 import panorama.command.DeadlineCommand;
+import panorama.command.DeleteCommand;
 import panorama.command.EventCommand;
+import panorama.command.MarkCommand;
 import panorama.command.TodoCommand;
+import panorama.command.UnmarkCommand;
 import panorama.exception.EmptyDescriptionException;
 import panorama.exception.UnknownCommandException;
 
@@ -62,6 +65,21 @@ public class Parser {
             LocalDate from = DateParser.parse(dateRangeTokens[0]);
             LocalDate to = DateParser.parse(dateRangeTokens[1]);
             return new EventCommand(taskList, name, from, to);
+        }
+
+        case MarkCommand.COMMAND_WORD: {
+            int id = Integer.parseInt(rest) - 1;
+            return new MarkCommand(taskList, id);
+        }
+
+        case UnmarkCommand.COMMAND_WORD: {
+            int id = Integer.parseInt(rest) - 1;
+            return new UnmarkCommand(taskList, id);
+        }
+
+        case DeleteCommand.COMMAND_WORD: {
+            int id = Integer.parseInt(rest) - 1;
+            return new DeleteCommand(taskList, id);
         }
 
         default:
