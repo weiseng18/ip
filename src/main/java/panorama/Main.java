5cce6143e5487c9f030226fa1c7d6f5147e2a4f7
weiseng18 <tws.hci@gmail.com>
2024-09-10 01:44:41 +0800

    Fix checkstyle

--- src/main/java/panorama/Main.java
+++ src/main/java/panorama/Main.java
@@ -27,7 +27,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setDuke(duke);  // inject the Duke instance
+            fxmlLoader.<MainWindow>getController().setDuke(duke); // inject the Duke instance
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
