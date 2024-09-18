da67fbccc0e020f8d001180f4c5d132b491c6215
weiseng18 <tws.hci@gmail.com>
2024-09-19 00:13:33 +0800

    Add basic tests for TaskList

---
+++ src/test/java/panorama/TaskListTest.java
@@ -0,0 +1,50 @@
+package panorama; // same package as the class being tested
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import panorama.task.Task;
+import panorama.task.Todo;
+
+public class TaskListTest {
+    private TaskList taskList;
+
+    @BeforeEach
+    public void setup() {
+        // Initialize an empty TaskList before each
+        taskList = new TaskList(true);
+    }
+
+    @Test
+    public void taskList_initializeEmpty_success() {
+        List<Task> tasks = taskList.getTasks();
+        assertEquals(0, tasks.size());
+    }
+
+    @Test
+    public void taskList_initializeWithList_success() {
+        List<Task> presetTasks = new ArrayList<>();
+        presetTasks.add(new Todo("task 1"));
+        presetTasks.add(new Todo("task 2"));
+
+        // re-initialize
+        taskList = new TaskList(presetTasks, true);
+
+        List<Task> tasks = taskList.getTasks();
+        assertEquals(presetTasks.size(), tasks.size());
+    }
+
+    @Test
+    public void add_success() {
+        Todo t = new Todo("task 1");
+        taskList.add(t);
+
+        Task task = taskList.get(0);
+        assertEquals(t, task);
+    }
+}
